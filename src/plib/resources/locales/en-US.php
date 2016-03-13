53a5b84cefae62f57d0ff2bfd289c60560999680
Vladimir Likhtanskiy <vlikhtanskiy@plesk.com>
2016-03-13 06:58:23 +0100

    Fix typo

--- src/plib/resources/locales/en-US.php
+++ src/plib/resources/locales/en-US.php
@@ -3,7 +3,7 @@
 $messages = [
     'controllers.index.list.pageTitle' => 'Joomla installations',
     'controllers.index.register.pageTitle' => 'Register installation',
-    'controllers.index.register.pageHint' => 'This is temporary page. It should be renmoved after scan functionality implementation.',
+    'controllers.index.register.pageHint' => 'This is temporary page. It should be removed after scan functionality implementation.',
     'controllers.index.register.successMsg' => 'Installation was successfully registered',
 
     'components.list.installations.pathColumn' => 'Path',
