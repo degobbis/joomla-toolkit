bfa873aef0e3d21439164db624501880977ea27e
Vladimir Likhtanskiy <vlikhtanskiy@plesk.com>
2016-03-13 23:19:58 +0100

    Use sbin wrapper

--- src/plib/library/JoomlaCli/Update.php
+++ src/plib/library/JoomlaCli/Update.php
@@ -5,15 +5,11 @@ class Modules_JoomlaToolkit_JoomlaCli_Update
     public static function getInfo(Modules_JoomlaToolkit_Model_Row_Installation $installation)
     {
         $subscription = new pm_Domain($installation->subscriptionId);
-        // TODO: use correct PHP version and user access
-        $result = pm_ApiCli::callSbin('php', [
+        $result = Modules_JoomlaToolkit_CallSbinWrapper::callPhp(
             '/var/www/vhosts/' . $subscription->getName() . $installation->path . '/cli/update.php',
-            '--info',
-        ]);
-        if (0 != $result['code']) {
-            throw new pm_Exception('Cannot load extension info: ' . $result['stdout']);
-        }
-        return json_decode($result['stdout'], true);
+            ['--info']
+        );
+        return json_decode($result, true);
     }
 
     public static function checkUpdateScript($path)
