e02b16fb1cce9e0f84daef35f0c9571596346593
Vladimir Likhtanskiy <vlikhtanskiy@plesk.com>
2016-03-14 14:56:33 +0100

    Custom button for admin

--- src/plib/hooks/CustomButtons.php
+++ src/plib/hooks/CustomButtons.php
@@ -6,11 +6,19 @@ class Modules_JoomlaToolkit_CustomButtons extends pm_Hook_CustomButtons
     {
         return [
             [
+                'place' => self::PLACE_COMMON,
+                'title' => pm_Locale::lmsg('components.customButtons.adminJoomlaToolkit'),
+                'description' => pm_Locale::lmsg('components.customButtons.adminJoomlaToolkitDescription'),
+                //'icon' => pm_Context::getBaseUrl() . 'images/icon.png', // TODO: add icon
+                'link' => pm_Context::getActionUrl('index', 'list'),
+                'hidden' => !pm_Session::getClient()->isAdmin(),
+            ], [
                 'place' => self::PLACE_DOMAIN,
                 'title' => pm_Locale::lmsg('components.customButtons.customerJoomlaToolkit'),
                 'description' => pm_Locale::lmsg('components.customButtons.customerJoomlaToolkitDescription'),
                 //'icon' => pm_Context::getBaseUrl() . 'images/icon.png', // TODO: add icon
                 'link' => pm_Context::getActionUrl('index', 'list'),
+                'hidden' => !pm_Session::getClient()->isClient(),
             ],
         ];
     }
